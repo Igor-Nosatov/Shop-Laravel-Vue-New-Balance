fbad6dc42a5c4720a4b330569f6425aaca66a9db
Igor_Nosatov <nosatov_ik@groupbwt.com>
2022-08-05 22:24:45 +0300

    feat: add store template

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,50 +1,61 @@
-import { createRouter, createWebHistory } from 'vue-router/dist/vue-router.esm-bundler'
-import Home1 from './views/Home1.vue'
-import Home from './views/Home/Home.vue'
-import Login from './views/Auth/Login.vue'
-import Register from './views/Auth/Register.vue'
+import {
+  createRouter,
+  createWebHistory,
+} from "vue-router/dist/vue-router.esm-bundler";
+import Home1 from "./views/Home1.vue";
+import Home from "./views/Home/Home.vue";
+import Login from "./views/Auth/Login.vue";
+import Register from "./views/Auth/Register.vue";
+import Store from "./views/Store/Store.vue";
 
-export default () => createRouter({
-  history: createWebHistory(),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: Login,
-    },
-    {
-      path: '/register',
-      name: 'register',
-      component: Register,
-    },
+export default () =>
+  createRouter({
+    history: createWebHistory(),
+    routes: [
+      {
+        path: "/",
+        name: "home",
+        component: Home,
+      },
+
+      {
+        path: "/login",
+        name: "login",
+        component: Login,
+      },
+
+      {
+        path: "/register",
+        name: "register",
+        component: Register,
+      },
+
+      {
+        path: "/store",
+        name: "store",
+        component: Store,
+      },
 
 
 
 
+      
 
-
-
-
-    {
-      path: '/home1',
-      name: 'home1',
-      component: Home1,
-    },
-    {
-      path: '/new',
-      name: 'new',
-      component: () => import('./views/NewCompany.vue')
-    },
-    {
-      path: '/company/:id/edit',
-      name: 'edit',
-      component: () => import('./views/EditCompany.vue'),
-      props: true
-    },
-  ]
-})
+      {
+        path: "/home1",
+        name: "home1",
+        component: Home1,
+      },
+      {
+        path: "/new",
+        name: "new",
+        component: () => import("./views/NewCompany.vue"),
+      },
+      {
+        path: "/company/:id/edit",
+        name: "edit",
+        component: () => import("./views/EditCompany.vue"),
+        props: true,
+      },
+    ],
+  });
