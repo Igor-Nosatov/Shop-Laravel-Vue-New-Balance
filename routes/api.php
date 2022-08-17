0915143cd76315c8254c2058cc8d6c49bb73461f
Igor_Nosatov <nosatov_ik@groupbwt.com>
2022-08-17 23:08:05 +0300

    feat:add basic routing

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\CompanyController;
+use App\Http\Controllers\Api\Shop\CompanyController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +20,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('companies', CompanyController::class);
+
+Route::apiResource('cart', CartController::class);
+Route::apiResource('/', HomeController::class);
+Route::apiResource('cart', CheckoutController::class);
+Route::apiResource('gift-card', GiftCardController::class);
+Route::apiResource('store', StoreController::class);
+Route::apiResource('review', ReviewController::class);
+Route::apiResource('product', ProductController::class);
+Route::apiResource('auth', AuthController::class);
+Route::apiResource('account', AccountController::class);
